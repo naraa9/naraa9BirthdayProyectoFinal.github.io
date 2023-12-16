e3faa15c30d7c6e7838579e4e802ee9e995d69ef
naraa9 <152984054+naraa9@users.noreply.github.com>
2023-12-16 12:14:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+const birthdate =['12/04/1900', '13/12/2004'];
+const state = {
+  secret: birthdate[Math.floor(Math.random() * birthdate.length)],
+  grid: Array(6)
+    .fill()
+    .map(() => Array(10).fill('')),
+  currentRow: 0,
+  currentCol: 0,
+};
+
+function drawGrid(container) {
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 10; j++) {
+      drawBox(grid, i, j);
+    }
+  }
+
+  container.appendChild(grid);
+}
+
+function updateGrid() {
+  for (let i = 0; i < state.grid.length; i++) {
+    for (let j = 0; j < state.grid[i].length; j++) {
+      const box = document.getElementById(`box${i}${j}`);
+      box.textContent = state.grid[i][j];
+    }
+  }
+}
+
+function drawBox(container, row, col, letter = '') {
+  const box = document.createElement('div');
+  box.className = 'box';
+  box.textContent = letter;
+  box.id = `box${row}${col}`;
+
+  container.appendChild(box);
+  return box;
+}
+
+function registerKeyboardEvents() {
+  document.body.onkeydown = (e) => {
+    const key = e.key;
+    if (key === 'Enter') {
+      if (state.currentCol === 10) {
+        const word = getCurrentWord();
+          revealWord(word);
+          state.currentRow++;
+          state.currentCol = 0;
+
+      }
+    }
+    if (key === 'Backspace') {
+      removeLetter();
+    }
+    if (isLetter(key)) {
+      addLetter(key);
+    }
+
+    updateGrid();
+  };
+}
+
+function getCurrentWord() {
+  return state.grid[state.currentRow].reduce((prev, curr) => prev + curr);
+}
+
+
+function getNumOfOccurrencesInWord(word, letter) {
+  let result = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function getPositionOfOccurrence(word, letter, position) {
+  let result = 0;
+  for (let i = 0; i <= position; i++) {
+    if (word[i] === letter) {
+      result++;
+    }
+  }
+  return result;
+}
+
+function revealWord(guess) {
+  const row = state.currentRow;
+  const animation_duration = 500; // ms
+
+  for (let i = 0; i < 10; i++) {
+    const box = document.getElementById(`box${row}${i}`);
+    const letter = box.textContent;
+    const numOfOccurrencesSecret = getNumOfOccurrencesInWord(
+      state.secret,
+      letter
+    );
+    const numOfOccurrencesGuess = getNumOfOccurrencesInWord(guess, letter);
+    const letterPosition = getPositionOfOccurrence(guess, letter, i);
+
+    setTimeout(() => {
+      if (
+        numOfOccurrencesGuess > numOfOccurrencesSecret &&
+        letterPosition > numOfOccurrencesSecret
+      ) {
+        box.classList.add('empty');
+      } else {
+        if (letter === state.secret[i]) {
+          box.classList.add('right');
+        } else if (state.secret.includes(letter)) {
+          box.classList.add('wrong');
+        } else {
+          box.classList.add('empty');
+        }
+      }
+    }, ((i + 1) * animation_duration) / 2);
+
+    box.classList.add('animated');
+    box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
+  }
+
+  const isWinner = state.secret === guess;
+  const isGameOver = state.currentRow === 6;
+
+  setTimeout(() => {
+    if (isWinner) {
+      terminar('Felicidades ganaste!🤩🎊');
+    } else if (isGameOver) {
+      alert(`Better luck next time! The word was ${state.secret}.`);
+    }
+  }, 6 * animation_duration);
+}
+function terminar(mensaje){
+    let contenedor= document.getElementById("mensaje");
+    contenedor.innerHTML= mensaje;
+}
+function isLetter(key) {
+  return key.length === 1 && key.match(/[0-9-/]/i);
+}
+
+function addLetter(letter) {
+  if (state.currentCol === 10) return;
+  state.grid[state.currentRow][state.currentCol] = letter;
+  state.currentCol++;
+}
+
+function removeLetter() {
+  if (state.currentCol === 0) return;
+  state.grid[state.currentRow][state.currentCol - 1] = '';
+  state.currentCol--;
+}
+
+function startup() {
+  const game = document.getElementById('game');
+  drawGrid(game);
+
+  registerKeyboardEvents();
+}
+
+startup();
